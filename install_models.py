f544fb957d4bd7e2890c6b1eb62ef41412b6519d
Lujun LI <lujun.li@uni.lu>
2024-07-01 18:03:44 +0200

    update readme

--- install_models.py
+++ install_models.py
@@ -2,30 +2,35 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 
+# Get the value of the PROJECT_HOME environment variable
 project_home = os.getenv("PROJECT_HOME")
 print(project_home)
-# Dataset
 
+# Initialize an empty list to store dataset links
 dataset_links = []
 
+# Model parameters
+TOKEN = "XXX"  # Authentication token
+device = "cpu"  # Use CPU only for downloading the files
+model_id = "meta-llama/Llama-2-7b-chat-hf"  # ID of the model to be used
 
-# Model mistralai/Mixtral-8x7B-Instruct-v0.1
-
-TOKEN = "XXX"
-device = "cpu"  # CPU only for downloading the files
-model_id = "meta-llama/Llama-2-7b-chat-hf"
-
-# Indicate the cache directory
+# Load the tokenizer, indicating the cache directory
 tokenizer = AutoTokenizer.from_pretrained(
     model_id, use_auth_token=TOKEN, cache_dir=f"{project_home}/XXX/.cache"
 )
+
+# Load the model, indicating the cache directory
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    device_map="auto",
+    device_map="auto",  # Automatically map model layers to devices
     use_auth_token=TOKEN,
     cache_dir=f"{project_home}/XXX/.cache",
 )
-tokenizer.save_pretrained("/project/home/p200400/XXX/model/Llama-2-7b-hf")
+
+# Save the tokenizer to the specified directory
+tokenizer.save_pretrained("/project/home/XXX/XXX/model/Llama-2-7b-hf")
+
+# Save the model to the specified directory
 model.save_pretrained(
     "/project/home/XXX/XXX/model/Llama-2-7b-hf", safe_serialization=False
 )
--- readme.md
+++ readme.md
@@ -1,11 +1,3 @@
-<!--
- * @Author: Lujun LI lujun.li@uni.lu
- * @Date: 2024-06-01 16:40:05
- * @LastEditors: Lujun LI lujun.li@uni.lu
- * @LastEditTime: 2024-06-01 16:41:09
- * @FilePath: /temperature_eval/readme.md
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 # Temperature_eval
 
 This is a respository for evaluation and visualtion of the results obtaining from observations.
