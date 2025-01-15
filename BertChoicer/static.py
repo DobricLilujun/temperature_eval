bb42f2f12e26713fb3da7b7b7348c24936f9508b
Lujun LI <lilujun588588@gmail.com>
2025-01-15 15:35:03 +0100

    update static

---
+++ BertChoicer/static.py
@@ -0,0 +1,4 @@
+model_id = "Volavion/bert-base-multilingual-uncased-Temperature-CLS"
+
+performance_distribution = "/home/snt/projects_lujun/temperature_eval_github/temperature_eval/data/Bert/all_data_for_bert_training.csv"
+
